84b85821619637006bf561bc1bb7d01b0a41a987
Godwill Lawson <godwill.lawson@gmail.com>
2025-08-21 23:28:22 +0200

    💬 App.jsx > add my name

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Salut God
+          Hi Godwill Lawson
         </p>
       </div>
       <p className="read-the-docs">
