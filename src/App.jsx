6a64a69a8fb2e65cdbf40df491ca5c5d6422438f
Godwill Lawson <godwill.lawson@gmail.com>
2025-08-28 23:08:44 +0200

    ✨ F02: ORDERPAGE > Create order page + installation react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
+import { Route, Routes } from 'react-router-dom'
 import LoginPages from './components/pages/login/LoginPages'
+import OrderPage from './components/pages/order/OrderPage'
 
 function App() {
   
 
   return (
     
-    < LoginPages />
+    <Routes>
+      <Route path='/' element = {< LoginPages />} />
+      <Route path='/order' element = {< OrderPage />} />
+    </Routes>
     
   )
 }
---
+++ src/components/pages/order/OrderPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function OrderPage() {
+    //states
+
+
+    //comportements
+
+
+    //affichage(render)
+  return (
+    <div>
+      Orderpage
+    </div>
+  )
+}
