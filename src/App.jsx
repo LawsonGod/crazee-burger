e35394a27f42f2764c6bbf6ed1e89d79207e4b9f
Godwill Lawson <godwill.lawson@gmail.com>
2025-08-30 13:46:44 +0200

    ✨ F02: ERRORPAGE + APP > Create Error page + install routing system

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Route, Routes } from 'react-router-dom'
 import LoginPages from './components/pages/login/LoginPages'
 import OrderPage from './components/pages/order/OrderPage'
+import ErrorPage from './components/pages/error/ErrorPage'
 
 function App() {
   
@@ -10,6 +11,7 @@ function App() {
     <Routes>
       <Route path='/' element = {< LoginPages />} />
       <Route path='/order' element = {< OrderPage />} />
+      <Route path='*' element = {< ErrorPage />} />
     </Routes>
     
   )
