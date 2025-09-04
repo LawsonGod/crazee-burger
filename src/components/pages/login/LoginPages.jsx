07189690a342f1cb409dc7b3076d83bd5d8006e2
Godwill Lawson <godwill.lawson@gmail.com>
2025-09-04 14:42:05 +0200

    💄 F03: LOGINPAGES > add background image to the loginpage

--- src/components/pages/login/LoginPages.jsx
+++ src/components/pages/login/LoginPages.jsx
@@ -1,13 +1,29 @@
 import LoginForm from "./LoginForm";
+import styled from "styled-components";
+import background from "../../../assets/F03 burger-background.jpg"    
+
 
 export default function LoginPages() {
    
 
     //affichage(render)
   return (
-    <div>
+    <LoginPagesStyle>
       <LoginForm />
-    </div>
+    </LoginPagesStyle>
   )
 }
+
+const LoginPagesStyle = styled.div`
+min-height: 100vh;
+min-width: 100vw;
+display: flex;
+justify-content: center;
+align-items: center;
+background-image: url(${background});
+background-size: cover;
+background-position: center;
+background-repeat: no-repeat;
+  
+`;
 
