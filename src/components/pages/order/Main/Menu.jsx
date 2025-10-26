c660385401aae8a13eeb8595d3f883c52066efc3
Godwill Lawson <godwill.lawson@gmail.com>
2025-10-26 22:52:39 +0100

    🚧 F05: ORDERPAGE > Menu > Add grip display

--- src/components/pages/order/Main/Menu.jsx
+++ src/components/pages/order/Main/Menu.jsx
@@ -1,15 +1,39 @@
+import { useState } from "react";
 import styled from "styled-components";
+import { fakeMenu2 } from "../../../../fakeData/fakeMenu";
 
 export default function Menu() {
+   //States
+  const [menu, _setMenu] = useState(fakeMenu2);
+
+
+    //Comportements
+
+
+    //Affichage (render)
   return (
+   
     <MenuStyled>            
-        menu
+        {menu.map((produit) => {
+          return <div className="produit">{produit.title}</div>;
+        })}
     </MenuStyled>
   )
 }
 
 const MenuStyled = styled. div`
+    background: purple;
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 60px;
+    padding: 50px 50px 150px;
+    justify-items: center;
 
-     background: purple;
+    .produit {
+    background: red;
+    width: 240px;
+    height: 330px;
+    
+    }
   
 `;
