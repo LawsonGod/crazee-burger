2d177871f544a72263bf96200ed01f4692f7ac4f
Godwill Lawson <godwill.lawson@gmail.com>
2025-10-27 09:22:45 +0100

    💄 F05: ORDERPAGE > Menu > Update the background style

--- src/components/pages/order/Main/Menu.jsx
+++ src/components/pages/order/Main/Menu.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import styled from "styled-components";
 import { fakeMenu2 } from "../../../../fakeData/fakeMenu";
+import { theme } from "../../../../theme/index";
 
 export default function Menu() {
    //States
@@ -22,18 +23,20 @@ export default function Menu() {
 }
 
 const MenuStyled = styled. div`
-    background: purple;
-    display: grid;
-    grid-template-columns: repeat(4, 1fr);
-    gap: 60px;
-    padding: 50px 50px 150px;
-    justify-items: center;
+  background: ${theme.colors.background_white};
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-row-gap: 60px;  
+  padding: 50px 50px 150px;
+  justify-items: center;
+  box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
 
-    .produit {
-    background: red;
-    width: 240px;
-    height: 330px;
-    
-    }
+
+  .produit {
+  background: red;
+  width: 240px;
+  height: 330px;
+  
+  }
   
 `;
