ef0cffca3f523ac68a51cc47c732f09b84d21878
Godwill Lawson <godwill.lawson@gmail.com>
2025-11-03 00:05:26 +0100

    💄 F06: NAVBAR > right side : add spacing between the toggle_button and the profile

--- src/components/pages/order/Navbar/NavbarRightSide.jsx
+++ src/components/pages/order/Navbar/NavbarRightSide.jsx
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import Profile from "./Profile";
+import ToggleButton from "../../../reusable-ui/ToggleButton";
 
 export default function NavbarRightSide({username}) {
     //states
@@ -11,9 +12,10 @@ export default function NavbarRightSide({username}) {
     //affichage(render)
   return (
      <NavbarRightSideStyled>
-        {/* <div className="admin-button">
-          Admin button
-        </div> */}
+        <ToggleButton
+          labelIfUnchecked="ACTIVER LE MODE ADMIN"
+          labelIfChecked="DÉSACTIVER LE MODE ADMIN"
+        />
         <Profile username={username} />
     </NavbarRightSideStyled>
   )
@@ -24,7 +26,5 @@ const NavbarRightSideStyled = styled.div`
     align-items: center;
     padding-right: 50px;
 
-    .admin-button{
-      background: lightblue;
-    }
+
 `;
