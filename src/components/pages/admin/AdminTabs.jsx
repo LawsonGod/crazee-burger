b6cb6d9604e52edfa94fa8b4f4fde79197856237
Godwill Lawson <godwill.lawson@gmail.com>
2025-11-12 09:44:31 +0100

    ✨  F07: ADMIN > TAB > add Label

--- src/components/pages/admin/AdminTabs.jsx
+++ src/components/pages/admin/AdminTabs.jsx
@@ -2,6 +2,7 @@ import styled from 'styled-components';
 import Tab from '../../reusable-ui/Tab';
 import { FiChevronDown, FiChevronUp } from 'react-icons/fi';
 import { theme } from '../../../theme';
+import { AiOutlinePlus } from 'react-icons/ai';
 
 export default function AdminTabs({isCollapsed, setIsCollapsed}) {
     // States
@@ -16,10 +17,18 @@ export default function AdminTabs({isCollapsed, setIsCollapsed}) {
   return (
     <AdminTabsStyled>
         <Tab 
+          label=""
           Icon = {isCollapsed ? <FiChevronUp /> : <FiChevronDown />} 
           onClick={handleClick}  
           className={isCollapsed ? 'is-active' : ''}
         />
+        <Tab
+          label="Ajouter un produit" 
+          Icon = {< AiOutlinePlus />} 
+          onClick={handleClick}  
+          className={isCollapsed ? 'is-active' : ''}
+        />
+
     </AdminTabsStyled>
   )
 }
@@ -32,6 +41,10 @@ const AdminTabsStyled = styled.div`
       background:${theme.colors.background_dark};
       border-color: ${theme.colors.background_dark};
       color: ${theme.colors.white};
+    }
+
+    button{
+      margin-left: 1px;
     }
   
 `;
