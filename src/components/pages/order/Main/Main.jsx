813288be4afcc4d55e1636714fbc80ebe1013066
Godwill Lawson <godwill.lawson@gmail.com>
2025-11-26 10:15:46 +0100

    💄 F07: THEME > Using design system for the box-shadow

--- src/components/pages/order/Main/Main.jsx
+++ src/components/pages/order/Main/Main.jsx
@@ -31,7 +31,7 @@ const MainStyled = styled.div`
   background: ${theme.colors.background_white};
   border-bottom-right-radius: ${theme.borderRadius.extraRound};
   border-bottom-left-radius: ${theme.borderRadius.extraRound};
-  box-shadow: 0px 8px 20px 8px rgba(0, 0, 0, 0.2) inset;
+  box-shadow: ${theme.shadows.strong};
   display: grid;
   grid-template-columns: /* 25% */ 1fr; 
   
